77a144746cd012733bc4c81666277131426ecadc
Szalma369 <marton.szalok@gmail.com>
2026-02-01 21:38:11 +0100

    add daily_product_category transformation and unit test files

---
+++ tests/etl/gold/test_daily_product_category.py
@@ -0,0 +1,53 @@
+from decimal import Decimal
+import pyspark.sql.types as st
+import pyspark.testing as spark_testing
+from cubix_data_engineer_capstone.etl.gold.daily_product_category import get_daily_product_category
+
+
+def test_get_daily_product_category(spark):
+    '''
+    Positive test that the function get_daily_product_category returns the expected DataFrame.
+    '''
+
+    wide_sales_test_data = [
+        ('english_name_1', Decimal('10.00'), Decimal('20.00')),
+        ('english_name_1', Decimal('16.00'), Decimal('26.00')),
+        ('english_name_1', Decimal('10.00'), Decimal('20.00')),
+        ('english_name_2', Decimal('20.00'), Decimal('40.00')),
+        ('english_name_2', Decimal('60.00'), Decimal('80.00')),
+    ]
+    wide_sales_test_schema = st.StructType([
+        st.StructField('EnglishProductCategoryName', st.StringType(), True),
+        st.StructField('SalesAmount', st.DecimalType(10, 2), True),
+        st.StructField('Profit', st.DecimalType(10, 2), True),
+    ])
+    wide_sales_test = spark.createDataFrame(wide_sales_test_data, schema=wide_sales_test_schema)
+
+    result = get_daily_product_category(wide_sales_test)
+
+    expected_schema = st.StructType([
+        st.StructField('EnglishProductCategoryName', st.StringType(), True),
+        st.StructField('SalesAmountSum', st.DecimalType(10, 2), True),
+        st.StructField('SalesAmountAvg', st.DecimalType(10, 2), True),
+        st.StructField('ProfitSum', st.DecimalType(10, 2), True),
+        st.StructField('ProfitAvg', st.DecimalType(10, 2), True),
+    ])
+    expected_data = [
+        (
+            'english_name_1',
+            Decimal('36.00'),
+            Decimal('12.00'),
+            Decimal('66.00'),
+            Decimal('22.00'),
+        ),
+        (
+            'english_name_2',
+            Decimal('80.00'),
+            Decimal('40.00'),
+            Decimal('120.00'),
+            Decimal('60.00'),
+        )
+    ]
+    expected = spark.createDataFrame(expected_data, schema=expected_schema)
+
+    spark_testing.assertDataFrameEqual(result, expected)
